82dbbf5b7b659259cc4561a0bf68a801d22e7c2e
Gadiel Monteabaro <silvagadiel96@gmail.com>
2025-09-13 17:25:08 -0300

    desarrollo de clase usuario y automatizacion

---
+++ src/app/automatizacion.py
@@ -0,0 +1,26 @@
+class Automatizacion:
+    def __init__(self, id, nombre, descripcion, estado="inactiva"):
+        self.id = id
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.estado = estado
+
+    def activar(self, dispositivos):
+
+        if self.estado != "activa":
+            print("La automatización no está activada.")
+            return
+
+        if self.nombre == "Encender todos los dispositivos":
+            for dispositivo in dispositivos:
+                # metodo creado en la clase dispositivo
+                dispositivo.encender_dispostivo()
+            print(f"Todos los dispositivos han sido encendidos")
+
+        if self.nombre == "Apagar todos los dispositivos":
+            for dispositivo in dispositivos:
+                # metodo creado en la clase dispositivo
+                dispositivo.apagar_dispostivo()
+            print(f"Todos los dispositivos han sido apagados")
+
+        # Se puede agregar más automatizaciones como la de monitoreo de limpieza.
---
+++ src/services/automatizacion_service.py
@@ -0,0 +1,58 @@
+from app.automatizacion import Automatizacion
+
+
+class AutomatizacionService:
+    def __init__(self):
+        self.automatizaciones = []
+        self.id_contar = 1
+
+    def nueva_automatizacion(self, id, nombre, descripcion):
+        nueva_automatizacion = Automatizacion(
+            id, nombre.capitalize(), descripcion.capitalize()
+        )
+        return nueva_automatizacion
+
+    def agregar_automatizacion(self, nombre, descripcion):
+        if not nombre or not descripcion:
+            raise ValueError("El nombre y la descripción son obligatorios")
+
+        for automatizacion in self.automatizaciones:
+            if automatizacion.nombre.lower() == nombre.lower():
+                return False, "La automatización ya existe."
+
+        nueva_automatizacion = self.nueva_automatizacion(
+            str(self.id_contar), nombre, descripcion
+        )
+        self.automatizaciones.append(nueva_automatizacion)
+        self.id_contar += 1
+
+        return True, "La automatización fue agregada exitosamente."
+
+    def eliminar_automatizacion(self, nombre):
+        for automatizacion in self.automatizaciones:
+            if automatizacion.nombre.lower() == nombre.lower():
+                self.automatizaciones.remove(automatizacion)
+                print("La automatización fue eliminada exitosamente.")
+                return
+        print("La automatización no fue encontrada.")
+
+
+if __name__ == "__main__":
+    servicio = AutomatizacionService()
+    servicio.agregar_automatizacion(
+        "Encender dispositivos", "Encender todos los dispositivos conectados"
+    )
+
+    servicio.agregar_automatizacion(
+        "Apagar dispositivos", "Apagar todos los dispositivos conectados"
+    )
+
+    print("Automatizaciones:")
+    for automatizacion in servicio.automatizaciones:
+        print(f"- {automatizacion.nombre}: {automatizacion.descripcion}")
+
+
+"""
+cd C:/Users/Silva/Desktop/Gadiel/Modulos/dispositivos/src
+python -m services.automatizacion_service
+"""
