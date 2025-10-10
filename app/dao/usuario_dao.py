ce1a5f77f62ea15f0e5ba8e09fa4eca4e84344bb
Gadiel Monteabaro <silvagadiel96@gmail.com>
2025-10-10 19:09:45 -0300

    feat: agregar metodos update_rol_user y get_all

--- app/dao/usuario_dao.py
+++ app/dao/usuario_dao.py
@@ -23,10 +23,36 @@ class UsuarioDAO:
             except mysql.connector.Error as err:
                 print(f"Error: {err}")
 
+    def update_rol_user(self, id_usuario, nuevo_rol):
+        with self.connection.cursor() as cursor:
+            try:
+                query = "UPDATE Usuario SET rol = %s WHERE id_usuario = %s"
+                values = (
+                    nuevo_rol,
+                    id_usuario,
+                )
+                cursor.execute(query, values)
+                self.connection.commit()
+            except mysql.connector.Error as err:
+                print(f"Error: {err}")
+
+    def get_all(self):
+        with self.connection.cursor() as cursor:
+            try:
+                query = "SELECT id_usuario, nombre, email, telefono,  rol FROM Usuario"
+                cursor.execute(query)
+                rows = cursor.fetchall()
+                usuarios = [
+                    Usuario(id=r[0], nombre=r[1], email=r[2], telefono=r[3], rol=r[4])
+                    for r in rows
+                ]
+                return usuarios
+            except mysql.connector.Error as err:
+                raise Exception(f"Error al listar usuarios: {err}")
+
     def get_user(self, nombre):
         with self.connection.cursor() as cursor:
             try:
-
                 query = "SELECT id_usuario, nombre, email, telefono, contraseña, rol FROM Usuario WHERE nombre = %s"
                 cursor.execute(query, (nombre,))
                 row = cursor.fetchone()
