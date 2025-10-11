61d11f348bf76ee9023d236da20930bc1755dacb
Gadiel Monteabaro <silvagadiel96@gmail.com>
2025-10-10 22:35:10 -0300

    refactor:corregir interface usuario

--- app/dao/interfaces/i_dispositivo_dao.py
+++ app/dao/interfaces/i_dispositivo_dao.py
@@ -3,25 +3,20 @@ from abc import ABC, abstractmethod
 class IDispositivoDAO(ABC):
     @abstractmethod
     def create_device(self, dispositivo, id_usuario):
-        """Crea un nuevo dispositivo y lo asocia a un usuario"""
         pass
 
     @abstractmethod
     def get_all(self):
-        """Devuelve todos los dispositivos"""
         pass
 
     @abstractmethod
     def update(self, dispositivo):
-        """Actualiza los datos de un dispositivo existente"""
         pass
 
     @abstractmethod
     def get_device_by_user(self, id_usuario):
-        """Devuelve todos los dispositivos asociados a un usuario"""
         pass
 
     @abstractmethod
     def delete(self, id_dispositivo):
-        """Elimina un dispositivo y su relación con el usuario"""
         pass
--- app/dao/interfaces/i_usuario_dao.py
+++ app/dao/interfaces/i_usuario_dao.py
@@ -3,20 +3,16 @@ from abc import ABC, abstractmethod
 class IUsuarioDAO(ABC):
     @abstractmethod
     def create(self, usuario):
-        """Crea un nuevo usuario"""
         pass
 
     @abstractmethod
     def update_rol_user(self, id_usuario, nuevo_rol):
-        """Actualiza el rol de un usuario"""
         pass
 
     @abstractmethod
     def get_all(self):
-        """Devuelve todos los usuarios"""
         pass
 
     @abstractmethod
     def get_user(self, nombre):
-        """Busca un usuario por nombre"""
         pass
